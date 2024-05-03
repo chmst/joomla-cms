383510cca7bf4d779fe0b50dd4718e113fcf958a
chmst <dev@chmst.de>
2024-05-03 21:12:03 +0200

    update menu item featured

--- administrator/components/com_admin/sql/updates/mysql/5.1.1-2024-04-18.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.1.1-2024-04-18.sql
@@ -1,10 +1,5 @@
---
--- Add a default value for the colorScheme in the Atum template on Joomla update
--- only when a value is not already set.
--- New installs will have the default value set in the installation sql.
---
-
-UPDATE `#__template_styles`
-SET `params` = JSON_SET(`params`, '$.colorScheme', 'os')
-WHERE `template` = 'atum'
-AND JSON_EXTRACT(`params`, '$.colorScheme') IS NULL;
+-- Update link to featured
+UPDATE `#__menu`
+   SET `link` = 'index.php?option=com_content&view=articles&featured=1'
+ WHERE `link` = 'index.php?option=com_content&view=featured'
+   AND `client_id` = 1;
--- administrator/components/com_admin/sql/updates/postgresql/5.1.1-2024-04-18.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.1.1-2024-04-18.sql
@@ -1,10 +1,5 @@
---
--- Add a default value for the colorScheme in the Atum template on Joomla update
--- only when a value is not already set.
--- New installs will have the default value set in the installation sql.
---
-
-UPDATE "#__template_styles"
-SET "params" = jsonb_set("params"::jsonb, '{colorScheme}', '"os"', true)
-WHERE "template" = 'atum'
-AND "params"::jsonb->>'colorScheme' IS NULL;
+-- Update link to featured
+UPDATE "#__menu"
+   SET "link" = 'index.php?option=com_content&view=articles&featured=1'
+ WHERE "link" = 'index.php?option=com_content&view=featured'
+   AND "client_id" = 1;
