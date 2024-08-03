68ff3cb2714e4cc4b8cb2137ac35b64df466ea25
chmst <dev@chmst.de>
2024-08-03 21:51:51 +0200

    Change menu liks from featured to articles, featured=1

---
+++ administrator/components/com_admin/sql/updates/postgresql/5.2.0-2024-08-03.sql
@@ -0,0 +1,8 @@
+--
+-- The featured View is integrated into articles view in Joomla 5.2.0
+--
+
+UPDATE "#__menu"
+SET "link" = 'index.php?option=com_content&view=articles&featured=1'
+WHERE "link" = 'index.php?option=com_content&view=featured'
+AND "client_id" = 1;
